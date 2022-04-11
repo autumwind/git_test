0b81b775303a008f76b04aaad77f719cb30cc666
Bing <401241397@qq.com>
2022-04-11 15:50:10 +0800

    push commit

--- src/main/java/com/atguigu/gittest.java
+++ src/main/java/com/atguigu/gittest.java
@@ -5,5 +5,6 @@ public class gittest {
         System.out.printf("hello git");
         System.out.println("hello git2");
         System.out.println("hello git23");
+        System.out.println("push test");
     }
 }
