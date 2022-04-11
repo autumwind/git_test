b52af465847dcc7f4ceff8aef392f463c01e7836
Bing <401241397@qq.com>
2022-04-11 14:50:15 +0800

    third commit

--- src/main/java/com/atguigu/gittest.java
+++ src/main/java/com/atguigu/gittest.java
@@ -4,5 +4,6 @@ public class gittest {
     public static void main(String[] args) {
         System.out.printf("hello git");
         System.out.println("hello git2");
+        System.out.println("hello git23");
     }
 }
